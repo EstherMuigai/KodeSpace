55e74c663d2c850c5e88c3d5c75ae4570c8fac27
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-04-01 11:18:34 +0300

    add styles to the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  inboundClick = true;
+  outboundClick = true;
   constructor() { }
 
   ngOnInit() {
