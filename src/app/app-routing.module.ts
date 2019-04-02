423ae791c8f9dfb1d74da91eb5b3fc49e941763e
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-04-02 23:00:42 +0300

    add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { GithubSearchComponent } from './github-search/github-search.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes:Routes=[
+  {path:"home",component:GithubSearchComponent},
+  {path:"profile",component:ProfileComponent},
+  {path:"about",component:AboutComponent},
+  {path:"",redirectTo:"/profile",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import {RouterModule,Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,13 +13,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TruncateTextDirective } from './truncate-text.directive';
 
-const routes:Routes=[
-  {path:"home",component:GithubSearchComponent},
-  {path:"profile",component:ProfileComponent},
-  {path:"about",component:AboutComponent},
-  {path:"",redirectTo:"/home",pathMatch:"full"},
-  {path:'**',component:NotFoundComponent}
-]
 
 @NgModule({
   declarations: [
@@ -36,7 +29,7 @@ const routes:Routes=[
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
